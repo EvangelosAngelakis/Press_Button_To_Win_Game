d68ecf4347e2e70ba8779d6e7cf3a34a1564c983
Evangelos Angelakis <Evangelosangel@gmail.com>
2024-11-27 15:35:01 +0000

    Started building JS

---
+++ Cycle_through_Funny_Messages.js
@@ -0,0 +1,11 @@
+const links = [
+    {text:'I said the correct button. No this one.'}
+    {text:'If you want to win you need to press the CORRECT button.'}
+    {text:'You are not very good at this, are you?'}
+    {text:'OMG, How hard is it to press the correct button?'}
+    {text:'I am starting to lose faith that you can actually do this.'}
+    {text:'Would you like help with this?'}
+    {text:'Even a baby can do this. Are you a Baby?'}
+    {text:'Oh come on, this is getting boring now.'}
+];
+
