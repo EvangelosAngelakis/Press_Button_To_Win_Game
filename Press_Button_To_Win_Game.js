f2923dd14bf46c6ed8f64c50f18f3fe70d17eed3
Evangelos Angelakis <Evangelosangel@gmail.com>
2024-11-28 15:25:29 +0000

    Game Completed
    HTML reset page addition. CSS improvements to run on various devices. JS improvements when running various scenarios.

--- Press_Button_To_Win_Game.js
+++ Press_Button_To_Win_Game.js
@@ -1,33 +1,31 @@
 const messages = [
     "I said the correct button. No this one.",
-    "If you want to win you need to press the CORRECT button.",
+    "If you want to win, press the CORRECT button.",
     "You are not very good at this, are you?",
-    "OMG, How hard is it to press the correct button?",
-    "I am starting to lose faith that you can actually do this.",
-    "Would you like help with this?",
+    "Would you like help with this trivial task?",
     "Even a baby can do this. Are you a Baby?",
     "Oh come on, this is getting boring now.",
-    "Oops, wrong button! Try again.",
+    "Oops, wrong button! Try again...and again!",
     "Nope, that's not the one. Keep trying!",
-    "Almost there, but not quite.",
-    "Better luck next time!",
+    "OMG,I am starting to lose faith here.",
+    "Almost there, but not quite there yet.",
+    "Better luck next time! Keep going.",
     "Not this one either. You can do it!",
     "Hmm, still not the correct button.",
     "Keep pressing, you'll get it eventually!",
-    "Wrong again! Don't give up!",
+    "Wrong again! Don't give up! Keep trying.",
     "This isn't the right button. Try another one.",
     "You're getting closer, but this isn't it.",
-    "Nope, not this one. Keep going!",
+    "Nope, this is not the one. Keep going!",
     "Incorrect button. Try a different one.",
     "Not quite. Keep pressing those buttons!",
-    "Wrong choice. Try again!",
     "This isn't the correct button. Keep trying!",
     "Nope, that's not it. Don't lose hope!",
-    "Still not the right button. You can do it! lol",
-    "Incorrect again. Keep pressing!",
-    "Not this one. Try another button.",
-    "Almost there, pinky promise!",
-    "Just a little bit more",
+    "Still not the right button. You can do it!",
+    "Incorrect again. Keep pressing buttons!",
+    "No, Not this one. Try another button.",
+    "Almost there, I promise. Cross my heart!",
+    "Just a little bit more. You can do it!",
 ];
 
 let currentIndex = 0;
@@ -36,10 +34,18 @@ function cycleMessage() {
     const dText = document.getElementById('dText');
     dText.innerText = messages[currentIndex];
     currentIndex = (currentIndex + 1) % messages.length;
+    document.getElementById('dButton').style.backgroundColor = "bisque";
+    document.getElementById('dText').style.color = "black";
 };
 
 
 document.getElementById('dText').addEventListener('click', function(){
-document.getElementById('dText').textContent = "CONGRATULATIONS. YOU WON!!!"
+document.getElementById('dText').textContent = "CONGRATULATIONS. YOU WON!!! You are a Super Inteligent being from outer Space!"
+document.getElementById('dText').style.color = "white";
+document.getElementById('dButton').style.backgroundColor = "red";
+clearTimeout(timeoutId);
 });
 
+function resetPage(){
+    location.reload()
+};
